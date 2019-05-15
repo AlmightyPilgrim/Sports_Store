59dac45da12c0390d3fc4c8589f5be559d68b1d7
Bret Hayes <bret_hayes@comcast.net>
2019-05-15 10:00:57 -0400

    Created unit test

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -11,10 +11,19 @@ namespace SportsStore.WebUI.Controllers
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        public int PageSize = 4;
         
         public ProductController(IProductRepository productRespository)
         {
             this.repository = productRespository;
+        }
+
+        public ViewResult List(int page = 1)
+        {
+            return View(repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize));
         }
 
         public ViewResult List()
