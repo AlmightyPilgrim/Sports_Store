4945a368ef846bd59cdeef3b173a053597f2bed7
Bret Hayes <bret_hayes@comcast.net>
2019-05-21 10:47:38 -0400

    made new class, thing still wont work

---
+++ SportsStore.WebUI/Infrastructure.Binders/CartModelBinder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Infrastructure.Binders
+{
+    public class CartModelBinder : IModelBinder 
+    {
+        private const string sessionKey = "Car";
+
+    }
+}
